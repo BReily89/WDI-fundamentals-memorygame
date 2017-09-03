541f122ba702b19c433dd8aa56ff780e78f3f297
BReily89 <31258147+BReily89@users.noreply.github.com>
2017-09-03 17:51:09 -0400

    Add files via upload
    created game logic

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-console.log("up and running!");
-var cardOne = "queen";
+console.log("Up and running!");
+
+var cards =("queen", "queen", "king", "king");
+var cardsInPlay = [];
+var cardOne = "king";
+cardsInPlay.push(cardOne);
+console.log("user flipped"  +  cardOne);
 var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+cardsInPlay.push(cardTwo);
+console.log("user flipped"  +  cardTwo);
+
+if (cardsInPlay.length === 2) {
+    (cardsInPlay[0] === (cardsInPlay.push[1]));
+        alert("you found a match!");
+}
+
+else  if (cardsInPlay[0] === cardsInPlay.push[1]) {
+    alert("Sorry, try again.");
+}
+
