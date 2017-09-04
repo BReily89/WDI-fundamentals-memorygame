f7fe84b2847881ded54d66e27a7e61431e6b423c
BReily89 <31258147+BReily89@users.noreply.github.com>
2017-09-04 14:24:05 -0400

    Add files via upload
    Working toward unit 10

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-console.log("Up and running!");
-
-var cards =("queen", "queen", "king", "king");
+console.log("Up and Running!");
+var cards = ("queen", "queen", "king", "king");{
 var cardsInPlay = [];
 var cardOne = "king";
 cardsInPlay.push(cardOne);
-console.log("user flipped"  +  cardOne);
+console.log("user flipped" +  cardOne);
 var cardTwo = "queen";
 cardsInPlay.push(cardTwo);
-console.log("user flipped"  +  cardTwo);
+console.log("user flipped" +  cardTwo);
+}
 
-if (cardsInPlay.length === 2) {
+if (cardsInPlay.length === [2]) {
     (cardsInPlay[0] === (cardsInPlay.push[1]));
         alert("you found a match!");
 }
@@ -17,4 +17,23 @@ if (cardsInPlay.length === 2) {
 else  if (cardsInPlay[0] === cardsInPlay.push[1]) {
     alert("Sorry, try again.");
 }
+var cardsInPlay = [];
+var checkForMatch = function() {
+    if ( cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay [1] ) {
+            alert("You have found a match!");
+        }else {
+            alert("Sorry, try again.");
+    }
+};
 
+
+var flipCard = function() {
+console.log("The user flipped a  " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId]);
+};
+
+flipCard(0);
+flipCard(2);
+
+// still trying to get cards to execute function
